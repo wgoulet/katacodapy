4139578fe86b7fcba04915e1351086937cf60a20
Walter Goulet <walter.goulet@venafi.com>
2019-11-14 13:34:00 -0600

    save file to disk

--- pyexample/scripts/createCourse.sh
+++ pyexample/scripts/createCourse.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-curl https://releases.hashicorp.com/vault/1.3.0/vault_1.3.0_linux_amd64.zip
+curl https://releases.hashicorp.com/vault/1.3.0/vault_1.3.0_linux_amd64.zip -O
 unzip vault_1.3.0_linux_amd64.zip
